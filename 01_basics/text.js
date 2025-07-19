19acf76450bd3c3ef207dffd031ecc1408b6a1b3
Rachit07cr <rachit.tripathi.cd.mec23@itbhu.ac.in>
2025-07-19 11:00:41 +0000

    lecture part 1 tk

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,24 @@
-console.log("rachit")
+const accountId=12345
+let accountemail="rachittripathi07@hmail.com"
+// agar let or var me changes kar skte hai to alag alag usse kyu kar rhe hain
+/*
+prefer not to use var because of block scope mtlb var me aisa scene tha ki tu khi bhi kisi var ko change karge
+to wo var same naam wla hr jgh change ho jaega mtlb scope define nhi tha dikkat ki baat thi 
+islie let or const hi use karne hain
+*/
+var accountpassword="2345"
+accountcity="jaipur". //java script me bina variable define kiye bhi bna skte hain but not a good practise
+// accountId=2
+
+
+accountemail="rachit.tripathi.cd.mec23@itbhu.ac.in"
+accountpassword="3242"
+accountcity="bengal"
+let accountstate;
+//const kyu nhi because user to user account state change ho skta hai
+//or agar koi bhi value pehle se na do to wo undefined store hoti hai javascript me
+
+console.table([accountId,accountemail,accountpassword,accountcity])
+// console.log(accountId);
+
+
