eeec589aea26220ed2b6015be73eec2b06706ae3
Rachit07cr <rachit.tripathi.cd.mec23@itbhu.ac.in>
2025-07-29 00:20:39 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,1 +1,26 @@
 console.log(2>=1)
+
+console.log("2">1)
+//ye kuch bhi aa skta hai isiliye dhyan dena ki hamesha same datatype ko hi compare karen\// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//aise comparisons ko avoid karenn
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
