170600bb57da353379064233db04ead2cad485ad
Rachit07cr <rachit.tripathi.cd.mec23@itbhu.ac.in>
2025-07-30 00:40:49 +0530

    Create datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123') //symbol ka use unique values rakhne ke liye kiya jaata hai
+const anotherId = Symbol('123')
+//ab jaise tumne sma value di hai dono ko but because symbol lga hai to unique hi rahenge dono 
+
+console.log(id === anotherId);
+//therefor false dena chiye isse
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
